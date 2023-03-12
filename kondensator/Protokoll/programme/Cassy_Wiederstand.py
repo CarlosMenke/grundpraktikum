467a65d0cb485cfba6c0328be8a8079c47bbc33a
CarlosMenke <carlos.menke@t-online.de>
2023-03-12 10:48:46 +0100

    berechne statistischen Fehler.

--- kondensator/Protokoll/programme/Cassy_Wiederstand.py
+++ kondensator/Protokoll/programme/Cassy_Wiederstand.py
@@ -142,7 +142,22 @@ for filename in sorted(os.listdir(cassy_dir)):
         daten = get_messdaten(cassy_dir +filename, 'I_A1')
         stromstaerke_mean.append(np.mean(daten))
         stromstaerke_std.append(np.std(daten, ddof=1))
-stromstaerke_std = np.array(stromstaerke_std)/np.sqrt(12)
-spannung_std = np.array(spannung_std)/np.sqrt(12)
-wiederstand_messdaten = {'U': spannung_mean, 'U_std': spannung_std, 'I': stromstaerke_mean, 'I_std': stromstaerke_std}
+ 
+spannung_mean = np.array(spannung_mean)
+spannung_std = np.array(spannung_std)
+spannung_mean_std = spannung_std/np.sqrt(2001)
+ 
+stromstaerke_mean = np.array(stromstaerke_mean)
+stromstaerke_std = np.array(stromstaerke_std)
+stromstaerke_mean_std = stromstaerke_std/np.sqrt(2001)
+ 
+wiederstand_messdaten = {'U': spannung_mean, 'U_unischerheit': spannung_mean_std, 'U_std': spannung_std, 'I': stromstaerke_mean, 'I_unsicherheit': stromstaerke_mean_std,'I_std': stromstaerke_std}
 print(pd.DataFrame(wiederstand_messdaten))
+
+### gesmater statistischer Fehler
+digitalisierung_std = 20 / 2**12 / np.sqrt(12)
+spannung_stat = np.sqrt(digitalisierung_std**2 + spannung_std**2)
+stromstaerke_stat = np.sqrt(digitalisierung_std**2 + stromstaerke_std**2)
+stat = {'stat Spannung': spannung_stat, 'stat Stromstärke': stromstaerke_stat}
+
+print('statistischer Fehler: \n', pd.DataFrame(stat))

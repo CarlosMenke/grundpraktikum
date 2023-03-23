7c966ae50a877d2dd84addcab42f4c77d70827b4
Maximilian Carlos Menke <login@mcmenke.de>
2023-03-23 07:30:37 +0100

    add neue messreihen

--- Pendel/Protokoll/programme/auswertung.py
+++ Pendel/Protokoll/programme/auswertung.py
@@ -3,6 +3,8 @@
 Maxima_stange_1 = {1:3.138, 11:18.063, 20:32.935, 30:49.473, 40:66.058, 50:82.598, 60:99.190, 70:115.729, 79:130.651, 90:148.843, 101:167.079, 112:185.279}
 Maxima_stange_2 = {1:1.455, 10:16.382, 20:32.944, 29:47.857, 39:64.411, 50:82.628, 60:99.192, 69:114.075, 80:132.288, 90:148.857, 100:165.417, 111:183.626}
 Maxima_stange_3 = {1:1.528, 11:18.088, 20:33.022, 29:47.909, 39:64.471, 51:84.347, 60:99.269, 69:114.162, 79:130.7, 90:148.936, 101:167.164, 111:183.715}
+ 
+Maxima_gewicht_1 = {1:1.508, 11:18.062, 21:34.581}
 
 for i in Maxima_stange_1:
     if i == 2: continue
@@ -14,4 +16,8 @@ for i in Maxima_stange_2:
 
 for i in Maxima_stange_3:
     if i == 1: continue
-    print((Maxima_stange_3[1] - Maxima_stange_3[i])/ (i-1))
+    #print((Maxima_stange_3[1] - Maxima_stange_3[i])/ (i-1))
+
+for i in Maxima_gewicht_1:
+    if i == 1: continue
+    print((Maxima_gewicht_1[1] - Maxima_gewicht_1[i])/ (i-1))
